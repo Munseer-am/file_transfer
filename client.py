b27ea14b75036a85e18b72efeced09c90eae57fb
Munseer-am <munnamunseer7@gmail.com>
2022-07-06 18:45:39 +0530

    file_transfer

---
+++ client.py
@@ -0,0 +1,31 @@
+from msilib.schema import File
+import socket
+IP = "127.0.0.1"
+PORT = 4455
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
+FILE = "/home/munseer/code/python/Animals.py"
+def main():
+    """ Staring a TCP socket. """
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    """ Connecting to the server. """
+    client.connect(ADDR)
+    """ Opening and reading the file data. """
+    file = open(File, "r")
+    data = file.read()
+    """ Sending the filename to the server. """
+    client.send("FILE".encode(FORMAT))
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+    """ Sending the file data to the server. """
+    client.send(data.encode(FORMAT))
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+    """ Closing the file. """
+    file.close()
+    """ Closing the connection from the server. """
+    client.close()
+if __name__ == "__main__":
+    main()
+
